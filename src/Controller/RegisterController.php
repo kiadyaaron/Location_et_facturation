59e986fb08622f273cc65bfd2b6db22e9b9cc04c
Kiadyaaron <aaronkiady@gmail.com>
2025-04-25 09:00:19 +0300

    registerr

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,16 +8,22 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class RegisterController extends AbstractController{
-
+final class RegisterController extends AbstractController
+{
     private $entityManager;
+    private $passwordHasher;
 
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ) {
         $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
     }
+
     #[Route('/inscription', name: 'app_register')]
     public function index(Request $request): Response
     {
@@ -26,18 +32,24 @@ final class RegisterController extends AbstractController{
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
 
-            $user=$form->getData();
+            // Hash du mot de passe
+            $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
 
             $this->entityManager->persist($user);
             $this->entityManager->flush();
+
+            // Message flash et redirection
+            $this->addFlash('success', 'Inscription réussie. Vous pouvez maintenant vous connecter.');
 
             return $this->redirectToRoute('app_login');
         }
 
         return $this->render('register/index.html.twig', [
-            'form'=> $form->createView()
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -28,19 +28,20 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $email = $request->getPayload()->getString('email');
-
+        $email = $request->request->get('email');
+    
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $email);
-
+    
         return new Passport(
             new UserBadge($email),
-            new PasswordCredentials($request->getPayload()->getString('password')),
+            new PasswordCredentials($request->request->get('password')),
             [
-                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
                 new RememberMeBadge(),
             ]
         );
     }
+    
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
@@ -48,9 +49,8 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirection après connexion réussie
+        return new RedirectResponse($this->urlGenerator->generate('app_account'));
     }
 
     protected function getLoginUrl(Request $request): string
